e1bf054d078962767a9bd8a347cf0c6fe80a2df5
2636965138@qq.com <2636965138@qq.com>
2021-07-28 20:11:21 +0800

    前端项目

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Tabs, Badge } from 'antd-mobile';
+
+const tabs = [
+  { title: <Badge text={'3'}>First Tab</Badge> },
+  { title: <Badge text={'今日(20)'}>Second Tab</Badge> },
+  { title: <Badge dot>Third Tab</Badge> },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/*<header className="App-header">*/}
+      {/*  <p>*/}
+      {/*    BJ TV*/}
+      {/*  </p>*/}
+      {/*  <Pagination mode="pointer" total={5} current={2} style={{ marginBottom: '16px' }} />*/}
+      {/*</header>*/}
+      <div>
+        <Tabs tabs={tabs}
+              initialPage={1}
+              onChange={(tab, index) => { console.log('onChange', index, tab); }}
+              onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of first tab
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of second tab
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of third tab
+          </div>
+        </Tabs>
+      </div>
     </div>
   );
 }
